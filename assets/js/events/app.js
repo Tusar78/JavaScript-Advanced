94faf5804fe85ea5b96cb034f30f04108ae66361
Tusar <tusarahmed141@gmail.com>
2024-04-08 17:12:23 +0600

    Rect animation is done

--- assets/js/events/app.js
+++ assets/js/events/app.js
@@ -3,10 +3,38 @@
 // }
 
 // addEventListener
-const btn1 = document.getElementById('btn1');
+// const btn1 = document.getElementById('btn1');
 
-btn1.addEventListener('mouseenter', myFunc)
+// btn1.addEventListener('mouseenter', myFunc)
 
-function myFunc() {
-    console.log('Hello World');
+// function myFunc() {
+//     console.log('Hello World');
+// }
+
+
+const rectDiv = document.querySelector('.rect');
+const rectBtn = document.querySelector('.rectBtn');
+
+const rectEffect1 = () => {
+    console.log('Working');
+    rectDiv.classList.add('rect1'); 
 }
+const rectEffect2 = () => {
+    console.log('Working');
+    rectDiv.classList.add('rect2'); 
+}
+const rectEffect3 = () => {
+    console.log('Working');
+    rectDiv.classList.add('rect3'); 
+}
+const rectEffect4 = () => {
+    console.log('Working');
+    rectDiv.className = 'rect'
+}
+
+
+
+rectDiv.addEventListener('mouseover', rectEffect1);
+rectDiv.addEventListener('mousedown', rectEffect2);
+rectDiv.addEventListener('dblclick', rectEffect3);
+rectBtn.addEventListener('click', rectEffect4);
